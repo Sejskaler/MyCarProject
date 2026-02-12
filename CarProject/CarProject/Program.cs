3577e5d6227da87c067152fc512827cc36327c98
Sejskaler <sejskaler@hotmail.dk>
2026-02-12 11:01:20 +0100

    Initial commit - virkende kode men mangler beregning i Beregning function

--- CarProject/CarProject/Program.cs
+++ CarProject/CarProject/Program.cs
@@ -4,8 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-//test 2
+            Console.Write("benzin eller diesel?");
+            string braendstof = Console.ReadLine();
+            double kmPerLiter;
+            int kilometerStand;
+
+            //kmperliter
+            while (true)
+            {
+                Console.Write("Km per liter: ");
+                string kmPerLiterInput = Console.ReadLine();
+
+                if (double.TryParse(kmPerLiterInput, out kmPerLiter))
+                    break;
+
+                Console.WriteLine("Det er ikke et nummer");
+            }
+//kilometerstand
+            while (true)
+            {
+                Console.Write("Kilometerstand: ");
+                string kilometerstandInput = Console.ReadLine();
+
+                if (int.TryParse(kilometerstandInput, out kilometerStand))
+                    break;
+
+                Console.WriteLine("Det er ikke et nummer");
+            }
+            double pris = Beregning(kmPerLiter, braendstof);
+            Console.WriteLine($"{pris}");
+        }
+            static double Beregning(double kmperliter, string braendstof)
+        {
+            if (braendstof.ToLower() == "diesel")
+            {
+                double braendstofpris = 12.29;
+                return (braendstofpris * 2);
+            }
+            else if (braendstof.ToLower() == "benzin")
+            {
+                double braendstofpris = 13.49;
+                return (braendstofpris * 1);
+            }
+            return 1;
         }
     }
 }
--- CarProject/CarProject/obj/Debug/net10.0/CarProject.AssemblyInfo.cs
+++ CarProject/CarProject/obj/Debug/net10.0/CarProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc290ef4c87ba114af568ddbd8abcb1f48779c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
