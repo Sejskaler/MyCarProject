5afadc1403e925b23857684a9ef80e4ab38995dd
Sejskaler <sejskaler@hotmail.dk>
2026-02-19 11:21:33 +0100

    Refactor, added alt til readcardetails som laver en touble af alt

--- CarProject/CarProject/Program.cs
+++ CarProject/CarProject/Program.cs
@@ -6,13 +6,80 @@ namespace CarProject
     {
         static void Main(string[] args)
         {
-            double distance;
-            double kmPerLiter;
-            int kilometerStand;
             double braendstofspris = 0;
             double pris = 0;
 
+                //hvis det er, så beregn prisen
 
+
+                var bil = ReadCarDetails();
+
+
+                pris = BeregnPrisAfBraendstof(bil.braendstof, bil.kmPerLiter, braendstofspris, bil.distance);
+
+            /*//skriv det hele ud
+            Console.WriteLine($"Brændstofstype: {braendstof}, km/l: {kmPerLiter}, oprindelig km stand: {kilometerStand} ny kilometerstand {kilometerStand + distance} det koster: {Math.Round(pris)}");
+
+            //try with string  format, it takes {0} etc. as the variables IN ORDER
+            Console.WriteLine(String.Format("Brændstofudgifterne for {0} km er {1} DKK.", distance, Math.Round(pris)));
+            //Overskrift
+            Console.WriteLine("Brændstof".PadRight(15) + "Distance".PadRight(15) + "Pris".PadRight(15));
+            //add linjer
+            Console.WriteLine(new string('-', 35));
+
+            //rækker
+            Console.WriteLine(braendstof.PadRight(15) + distance.ToString().PadRight(15) + Math.Round(pris).ToString().PadRight(15));
+            */
+        }
+
+        //lav beregningerne baseret på det der bliver parsed ind og return prisen
+            static double BeregnPrisAfBraendstof(string braendstof, double kmperliter, double braendstofspris, double distance)
+            {
+            switch (braendstof.ToLower())
+            {
+                case "benzin":
+                    braendstofspris = 13.49;
+                    break;
+                case "diesel":
+                    braendstofspris = 12.29;
+                    break;
+                case "el":
+                    braendstofspris = 1.12;
+                    break;
+                default:
+                    Console.WriteLine("fejl");
+                    return;
+            }
+            double fuelNeeded = distance / kmperliter;
+                double Pris = fuelNeeded * braendstofspris;
+                return Pris;
+
+            }
+
+
+            static (string brand, string model, int year, char gear, string braendstof, double kmPerLiter, double distance) ReadCarDetails()
+        { 
+            string brand = "Toyota";
+            string model = "Corolla";
+            string year = "2020";
+            int yearINT = 0;
+            char gear = 'a';
+            double kmPerLiter = 0;
+            double distance = 0;
+
+
+            Console.WriteLine("Brand: "+ Console.ReadLine());
+                brand = Console.ReadLine();
+
+                Console.WriteLine("Model: ");
+                model = Console.ReadLine();
+
+                Console.WriteLine("Aar: ");
+                year = Console.ReadLine();
+                yearINT = int.Parse(year);
+
+            Console.WriteLine("gear: ");
+                gear = char.Parse(Console.ReadLine());
             //kmperliter - loopet er brugt 3 gange
             while (true)
             {
@@ -22,7 +89,7 @@ namespace CarProject
 
                 //tjek om input er double og output det til kmPerLiter hvis det er
                 if (double.TryParse(kmPerLiterInput, out kmPerLiter))
-                { 
+                {
                     //Stop loopet hvis det er
                     break;
                 }
@@ -54,7 +121,7 @@ namespace CarProject
 
 
             //gøres lidt anderledes med benz og diesel
-            Console.WriteLine("benzin, diesel eller diesel?");
+            Console.WriteLine("benzin, diesel eller el?");
             string braendstof = Console.ReadLine();
 
             while (true)
@@ -65,78 +132,17 @@ namespace CarProject
                     //så skriv det lige igen
                     Console.Write("skriv lige benzin, diesel eller el: ");
                     braendstof = Console.ReadLine();
+
                 }
                 else
                 {
-                //hvis det er, så beregn prisen
-                switch (braendstof.ToLower())
-                    {
-                        case "benzin":
-                            braendstofspris = 13.49;
-                            break;
-                        case "diesel":
-                            braendstofspris = 12.29;
-                            break;
-                        case "el":
-                            braendstofspris = 1.12;
-                            break;
-                        default:
-                            Console.WriteLine("fejl");
-                            return;
-                    }
-
+                    break;
                 }
-                pris = BeregnPrisAfBraendstof(kmPerLiter, braendstofspris, distance);
-                break;
+                
+
+                return (brand, model, yearINT, gear, braendstof, kmPerLiter, distance);
 
 
-            }
-            //skriv det hele ud
-            Console.WriteLine($"Brændstofstype: {braendstof}, km/l: {kmPerLiter}, oprindelig km stand: {kilometerStand} ny kilometerstand {kilometerStand + distance} det koster: {Math.Round(pris)}");
-
-            //try with string  format, it takes {0} etc. as the variables IN ORDER
-            Console.WriteLine(String.Format("Brændstofudgifterne for {0} km er {1} DKK.", distance, Math.Round(pris)));
-            //Overskrift
-            Console.WriteLine("Brændstof".PadRight(15) + "Distance".PadRight(15) + "Pris".PadRight(15));
-            //add linjer
-            Console.WriteLine(new string('-', 35));
-
-            //rækker
-            Console.WriteLine( braendstof.PadRight(15) + distance.ToString().PadRight(15) + Math.Round(pris).ToString().PadRight(15));
-
-        }
-
-        //lav beregningerne baseret på det der bliver parsed ind og return prisen
-            static double BeregnPrisAfBraendstof(double kmperliter, double braendstofpris, double distance)
-            {
-                double fuelNeeded = distance / kmperliter;
-                double Pris = fuelNeeded * braendstofpris;
-                return Pris;
-
-            }
-
-            static (string brand, string model, int year, char gear) Bilparametre()
-        { 
-            string brand = "Toyota";
-            string model = "Corolla";
-            string year = "2020";
-            int yearINT = 0;
-            char gear = 'a';
-
-            Console.WriteLine("Brand: "+ Console.ReadLine());
-                brand = Console.ReadLine();
-
-                Console.WriteLine("Model: ");
-                model = Console.ReadLine();
-
-                Console.WriteLine("Aar: ");
-                year = Console.ReadLine();
-                yearINT = int.Parse(year);
-
-            Console.WriteLine("gear: ");
-                gear = char.Parse(Console.ReadLine());
-
-            return (brand, model, yearINT, gear);
 
            //($"Dit Bilmaerke er {brand} og det er modellen {model}. Den kommer fra år {year} og den kører med {gear} gear");
         }
